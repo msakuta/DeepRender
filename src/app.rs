907252bcce6f57e3ddebece37f543006083762f2
msakuta <masahiro.sakuta@gmail.com>
2023-06-03 01:24:25 +0900

    Show a basic visualization of neural network

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,11 @@
-use eframe::egui::{
-    plot::{Line, PlotPoints},
-    widgets::plot::Plot,
+use eframe::{
+    egui::{
+        self,
+        plot::{Line, PlotPoints},
+        widgets::plot::Plot,
+        Frame, Ui,
+    },
+    epaint::{pos2, Color32, Pos2, Rect},
 };
 
 use crate::{loss_fn, matrix::Matrix, sigmoid, sigmoid_derive};
@@ -14,7 +19,9 @@ pub struct DeepRenderApp {
 
 impl DeepRenderApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        let train = [[0., 0., 0.], [0., 1., 1.], [1., 0., 1.], [1., 1., 1.]];
+        // let train = [[0., 0., 0.], [0., 1., 1.], [1., 0., 1.], [1., 1., 1.]];
+        // let train = [[0., 0., 0.], [0., 1., 0.], [1., 0., 0.], [1., 1., 1.]];
+        let train = [[0., 0., 0.], [0., 1., 1.], [1., 0., 1.], [1., 1., 0.]];
         let model = Matrix::new([[0.67, 0.95, 0.32]]);
         Self {
             train: train.to_vec(),
@@ -50,6 +57,57 @@ impl DeepRenderApp {
             .color(eframe::egui::Color32::from_rgb(100, 200, 100))
             .name("circle")
     }
+
+    fn paint_graph(&self, ui: &mut Ui) {
+        Frame::canvas(ui.style()).show(ui, |ui| {
+            let (response, painter) =
+                ui.allocate_painter(ui.available_size(), egui::Sense::hover());
+
+            let to_screen = egui::emath::RectTransform::from_to(
+                Rect::from_min_size(Pos2::ZERO, response.rect.size()),
+                response.rect,
+            );
+
+            for i in 0..self.model.cols() {
+                // let rect = Rect{ min: pos2(30., 30. + i as f32 * 30.), max: pos2(80., 50. + i as f32 * 30.) };
+                let center = pos2(30., 30. + i as f32 * 30.);
+                painter.circle(
+                    to_screen.transform_pos(center),
+                    10.,
+                    Color32::from_rgb((self.model[(0, i)] * 255.).min(255.).max(0.) as u8, 0, 0),
+                    (1., Color32::GRAY),
+                );
+            }
+
+            let center = pos2(100., 30. + self.model.cols() as f32 / 2.);
+            painter.circle(
+                to_screen.transform_pos(center),
+                10.,
+                Color32::from_rgb(255, 0, 0),
+                (1., Color32::GRAY),
+            );
+
+            for i in 0..self.model.cols() {
+                // let rect = Rect{ min: pos2(30., 30. + i as f32 * 30.), max: pos2(80., 50. + i as f32 * 30.) };
+                let soure = pos2(30., 30. + i as f32 * 30.);
+                let dest = pos2(100., 30.);
+                painter.line_segment(
+                    [
+                        to_screen.transform_pos(soure),
+                        to_screen.transform_pos(dest),
+                    ],
+                    (
+                        1.,
+                        Color32::from_rgb(
+                            (self.model[(0, i)] * 255.).min(255.).max(0.) as u8,
+                            0,
+                            0,
+                        ),
+                    ),
+                );
+            }
+        });
+    }
 }
 
 impl eframe::App for DeepRenderApp {
@@ -58,13 +116,28 @@ impl eframe::App for DeepRenderApp {
 
         self.learn_iter();
 
-        eframe::egui::SidePanel::right("side_panel").show(ctx, |ui| {
-            ui.label(format!("Loss: {}", loss_fn(&self.train, &self.model)));
-            ui.label(format!("Model: {}", self.model));
-        });
+        eframe::egui::SidePanel::right("side_panel")
+            .min_width(200.)
+            .show(ctx, |ui| {
+                ui.label(format!("Loss: {}", loss_fn(&self.train, &self.model)));
+                ui.label(format!("Model: {}", self.model));
+                for sample in &self.train {
+                    let input = Matrix::new_row(&sample[0..2]).hstack(&Matrix::ones(1, 1));
+                    let interm = &input * &self.model.t();
+                    let predict = interm.map(sigmoid);
+                    ui.label(format!("{} -> {}", Matrix::new_row(&sample[0..2]), predict));
+                }
+            });
+
+        egui::TopBottomPanel::bottom("graph")
+            .resizable(true)
+            .min_height(100.)
+            .show(ctx, |ui| {
+                self.paint_graph(ui);
+            });
 
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
-            let plot = eframe::egui::plot::Plot::new("plot");
+            let plot = Plot::new("plot");
             plot.show(ui, |plot_ui| {
                 plot_ui.line(self.loss_history());
             })
--- src/matrix.rs
+++ src/matrix.rs
@@ -5,58 +5,62 @@ use std::{
 
 #[derive(Clone, PartialEq, Debug)]
 pub(crate) struct Matrix {
-    row: usize,
-    col: usize,
+    rows: usize,
+    cols: usize,
     v: Vec<f64>,
 }
 
 impl Matrix {
     pub(crate) fn new<const R: usize, const C: usize>(m: [[f64; C]; R]) -> Self {
         Self {
-            row: R,
-            col: C,
+            rows: R,
+            cols: C,
             v: m.into_iter().flatten().collect(),
         }
     }
 
     pub(crate) fn zeros(row: usize, col: usize) -> Self {
         Self {
-            row,
-            col,
+            rows: row,
+            cols: col,
             v: vec![0.; row * col],
         }
     }
 
     pub(crate) fn ones(row: usize, col: usize) -> Self {
         Self {
-            row,
-            col,
+            rows: row,
+            cols: col,
             v: vec![1.; row * col],
         }
     }
 
     pub(crate) fn new_row(row: &[f64]) -> Self {
         Self {
-            row: 1,
-            col: row.len(),
+            rows: 1,
+            cols: row.len(),
             v: row.to_vec(),
         }
     }
 
     pub(crate) fn hstack(&self, other: &Self) -> Self {
-        assert_eq!(self.row, other.row);
-        let row = self.row;
-        let col = self.col + other.col;
-        let mut v = vec![0.; self.row * (self.col + other.col)];
-        for r in 0..self.row {
-            for c in 0..self.col {
-                v[r * col + c] = self.v[r * self.col + c];
+        assert_eq!(self.rows, other.rows);
+        let row = self.rows;
+        let col = self.cols + other.cols;
+        let mut v = vec![0.; self.rows * (self.cols + other.cols)];
+        for r in 0..self.rows {
+            for c in 0..self.cols {
+                v[r * col + c] = self.v[r * self.cols + c];
             }
-            for c in 0..other.col {
-                v[r * col + c + self.col] = other.v[r * other.col + c];
+            for c in 0..other.cols {
+                v[r * col + c + self.cols] = other.v[r * other.cols + c];
             }
         }
-        Self { row, col, v }
+        Self {
+            rows: row,
+            cols: col,
+            v,
+        }
     }
 
     pub(crate) fn eye(size: usize) -> Self {
@@ -65,62 +69,70 @@ impl Matrix {
             v[i * size + i] = 1.;
         }
         Self {
-            row: size,
-            col: size,
+            rows: size,
+            cols: size,
             v,
         }
     }
 
     pub(crate) fn t(&self) -> Self {
-        let mut v = vec![0.; self.row * self.col];
-        for r in 0..self.row {
-            for c in 0..self.col {
-                v[c * self.row + r] = self.v[r * self.col + c];
+        let mut v = vec![0.; self.rows * self.cols];
+        for r in 0..self.rows {
+            for c in 0..self.cols {
+                v[c * self.rows + r] = self.v[r * self.cols + c];
             }
         }
         Self {
-            row: self.col,
-            col: self.row,
+            rows: self.cols,
+            cols: self.rows,
             v,
         }
     }
 
     pub(crate) fn map(&self, f: impl Fn(f64) -> f64) -> Self {
         Self {
-            row: self.row,
-            col: self.col,
+            rows: self.rows,
+            cols: self.cols,
             v: self.v.iter().copied().map(f).collect(),
         }
+    }
+
+    pub(crate) fn rows(&self) -> usize {
+        self.rows
+    }
+
+    pub(crate) fn cols(&self) -> usize {
+        self.cols
     }
 }
 
 impl Index<(usize, usize)> for Matrix {
     type Output = f64;
     fn index(&self, (r, c): (usize, usize)) -> &Self::Output {
-        &self.v[r * self.col + c]
+        &self.v[r * self.cols + c]
     }
 }
 
 impl IndexMut<(usize, usize)> for Matrix {
     fn index_mut(&mut self, (r, c): (usize, usize)) -> &mut Self::Output {
-        &mut self.v[r * self.col + c]
+        &mut self.v[r * self.cols + c]
     }
 }
 
 impl Display for Matrix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "[ ")?;
-        for r in 0..self.row {
+        for r in 0..self.rows {
             if r != 0 {
                 write!(f, "  ")?;
             }
-            for c in 0..self.col {
-                write!(f, "{}", self.v[r * self.col + c])?;
-                if c != self.col - 1 {
+            for c in 0..self.cols {
+                write!(f, "{}", self.v[r * self.cols + c])?;
+                if c != self.cols - 1 {
                     write!(f, " ")?;
                 }
             }
-            if r != self.row - 1 {
+            if r != self.rows - 1 {
                 write!(f, "\n")?;
             }
         }
@@ -132,15 +144,15 @@ impl Display for Matrix {
 impl std::ops::Add for Matrix {
     type Output = Self;
     fn add(self, other: Matrix) -> Matrix {
-        assert_eq!(self.row, other.row);
-        assert_eq!(self.col, other.col);
+        assert_eq!(self.rows, other.rows);
+        assert_eq!(self.cols, other.cols);
         let mut v = self.v;
         for (v, o) in v.iter_mut().zip(other.v.iter()) {
             *v += o;
         }
         Self {
-            row: self.row,
-            col: self.col,
+            rows: self.rows,
+            cols: self.cols,
             v,
         }
     }
@@ -157,20 +169,20 @@ fn test_add() {
 impl std::ops::Mul for &Matrix {
     type Output = Matrix;
     fn mul(self, rhs: Self) -> Self::Output {
-        assert_eq!(self.col, rhs.row);
-        let mut v = vec![0.; self.row * rhs.col];
-        for r in 0..self.row {
-            for c in 0..rhs.col {
-                v[r * rhs.col + c] = self.v[r * self.col..(r + 1) * self.col]
+        assert_eq!(self.cols, rhs.rows);
+        let mut v = vec![0.; self.rows * rhs.cols];
+        for r in 0..self.rows {
+            for c in 0..rhs.cols {
+                v[r * rhs.cols + c] = self.v[r * self.cols..(r + 1) * self.cols]
                     .iter()
-                    .zip(0..self.col)
-                    .map(|(s, oi)| s * rhs.v[oi * rhs.col + c])
+                    .zip(0..self.cols)
+                    .map(|(s, oi)| s * rhs.v[oi * rhs.cols + c])
                     .fold(0., |acc, cur| acc + cur)
             }
         }
         Matrix {
-            row: self.row,
-            col: rhs.col,
+            rows: self.rows,
+            cols: rhs.cols,
             v,
         }
     }
